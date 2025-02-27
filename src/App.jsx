edec5fe37caa41db7f71f83b271f254f1f39d772
AyushaJadhav <130702125+AyushaJadhav@users.noreply.github.com>
2025-02-27 11:36:23 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import TaskBoard from "./components/TaskBoard";
 import "./index.css";
+import { initializeIcons } from "@fluentui/react";
+initializeIcons(); 
+
 
 function App() {
   return (
     <div className="bg-white-50 min-h-screen flex items-center justify-center">
+      
       <TaskBoard />
     </div>
+    
   );
 }
 
 export default App;
+
+
+
+
+
+
+
